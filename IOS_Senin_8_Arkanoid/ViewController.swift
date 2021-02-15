1ca71abf749a205ee2ee367e353e5d021ffdb49c
Evgeny Makhankov <EMakhankov@gmail.com>
2021-02-15 18:42:20 +0300

    Initial Commit

---
+++ IOS_Senin_8_Arkanoid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_Senin_8_Arkanoid
+//
+//  Created by Jenya on 15.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
